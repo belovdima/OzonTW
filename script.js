54e5710e9e0af2c09f616f5c4d8c64a39261541f
Dmitry <machete75111@gmail.com>
2025-02-08 18:47:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ let radius = progressCircle.r.baseVal.value;
 let circumference = radius * 2 * Math.PI;
 
 progressCircle.style.strokeDasharray = circumference;
-
+//формула расчёта в процентах
 function setProgress(percent) {
     progressCircle.style.strokeDashoffset =
         circumference - circumference * (percent / 100);
 }
 
-setProgress(50);
+const inputField = document.querySelector(".progress__input-field"); //получаем значение инпута
+//меняем значение инпута при его изменении
+inputField.addEventListener("input", function () {
+    let currentValue = inputField.value;
+    setProgress(currentValue);
+});
+//не позволяем больше 100 вводить
+inputField.addEventListener("input", function () {
+    if (parseInt(inputField.value) > 100) {
+        inputField.value = 100;
+    }
+});
 
+// функция отображения компонентов при повороте экрана
 function handleOrientationChange() {
     const progress = document.querySelector(".progress");
 
