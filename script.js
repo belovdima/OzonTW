5e0cece6b1f17667aeee9fe9238f0becec58c54b
Dmitry <machete75111@gmail.com>
2025-02-08 19:05:56 +0300

    update js

--- script.js
+++ script.js
@@ -3,26 +3,29 @@ let radius = progressCircle.r.baseVal.value;
 let circumference = radius * 2 * Math.PI;
 
 progressCircle.style.strokeDasharray = circumference;
-//формула расчёта в процентах
+
+// Функция установки прогресса с ограничением 0-100
 function setProgress(percent) {
+    if (percent > 100) {
+        percent = 100;
+        inputField.value = 100;
+    } else if (percent < 0 || inputField.value === "") {
+        percent = 0;
+        inputField.value = "";
+    }
+
     progressCircle.style.strokeDashoffset =
         circumference - circumference * (percent / 100);
 }
 
-const inputField = document.querySelector(".progress__input-field"); //получаем значение инпута
-//меняем значение инпута при его изменении
+const inputField = document.querySelector(".progress__input-field"); // Получаем значение инпута
+
+// Слушатель на изменение инпута
 inputField.addEventListener("input", function () {
-    let currentValue = inputField.value;
-    setProgress(currentValue);
-});
-//не позволяем больше 100 вводить
-inputField.addEventListener("input", function () {
-    if (parseInt(inputField.value) > 100) {
-        inputField.value = 100;
-    }
+    setProgress(parseInt(inputField.value));
 });
 
-// функция отображения компонентов при повороте экрана
+// Функция отображения компонентов при повороте экрана
 function handleOrientationChange() {
     const progress = document.querySelector(".progress");
 
